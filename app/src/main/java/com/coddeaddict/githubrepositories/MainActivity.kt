bce08dc8f616d998b9fe2760d0dfdbe007740575
alicja.99 <alicja.mruk99@gmail.com>
2020-12-04 23:51:55 +0100

    Configure navigation graph

--- app/src/main/java/com/coddeaddict/githubrepositories/MainActivity.kt
+++ app/src/main/java/com/coddeaddict/githubrepositories/MainActivity.kt
@@ -2,10 +2,29 @@ package com.coddeaddict.githubrepositories
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.coddeaddict.githubrepositories.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
+        setContentView(binding.root)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.repoListFragment))
+        val navController = findNavController(R.id.repoListFragment)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        if (!findNavController(R.id.repoListFragment).navigateUp()) finish()
+        return true
     }
 }

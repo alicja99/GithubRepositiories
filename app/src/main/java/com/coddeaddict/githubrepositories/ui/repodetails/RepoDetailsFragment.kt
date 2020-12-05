baf5d529de93a6f62c5d193530c3397605dfe074
alicja.99 <alicja.mruk99@gmail.com>
2020-12-05 09:29:35 +0100

    Add view binding configuration to both fragments

--- app/src/main/java/com/coddeaddict/githubrepositories/ui/repodetails/RepoDetailsFragment.kt
+++ app/src/main/java/com/coddeaddict/githubrepositories/ui/repodetails/RepoDetailsFragment.kt
@@ -5,19 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.coddeaddict.githubrepositories.R
+import com.coddeaddict.githubrepositories.databinding.FragmentRepoDetailsBinding
 
 
 class RepoDetailsFragment : Fragment() {
+    private lateinit var binding: FragmentRepoDetailsBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onPause() {
+        super.onPause()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_repo_details, container, false)
+    ): View {
+        binding = FragmentRepoDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
